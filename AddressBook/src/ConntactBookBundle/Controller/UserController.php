083d87b9aad4c9d00de756cdff02d8c17c756640
Arkadiusz Zezula <arkadiusz.zezula@gmail.com>
2017-06-03 18:01:24 +0200

    added modify contact

--- AddressBook/src/ConntactBookBundle/Controller/UserController.php
+++ AddressBook/src/ConntactBookBundle/Controller/UserController.php
@@ -29,11 +29,10 @@ class UserController extends Controller
             ->add("userSurname","text")
             ->add("userDescription","text")
             ->add("Create new ","submit")
-            ->getForm();
-        
+            ->getForm(); 
         // return form view 
-        return $this->render('ConntactBookBundle:User:newUser.html.twig', array(
-        "form" => $form->createView()
+        return $this->render('ConntactBookBundle:User:formUser.html.twig', array(
+        "form" => $form->createView(), "title" => "Add new Conntact"
         ));
     }
     
@@ -51,10 +50,8 @@ class UserController extends Controller
             ->add("userSurname","text")
             ->add("userDescription","text")
             ->getForm();
-        
         //handle request
         $form->handleRequest($request);
-        
         // save to DB
         if($form->isSubmitted()){
             $receivedNewConntact = $form->getData();
@@ -63,8 +60,46 @@ class UserController extends Controller
             $em->persist($receivedNewConntact);
             $em->flush();            
         }
-        
         return $this->redirectToRoute("/$idNewConntact");
     }
+     
+     
+    /**
+     * @Route("/{id}/modify")
+     
+     * @param Request $request
+     * @param type $id
+     * @return type* @Template()
+     */
+    public function createFormToModifyConntactAction(Request $request, $id) {
+        // find user in DB by send id
+        $er = $this->getDoctrine()->getRepository("ConntactBookBundle:User");
+        $modifyConntact = $er->find($id);
+        // create form to modify conntact
+        $form = $this->createFormBuilder($modifyConntact)
+            ->add("userName", "text")
+            ->add("userSurname","text")
+            ->add("userDescription","text")
+            ->add("Modify conntact ","submit")
+            ->getForm();
+        // update conntact in DB
+        if ($request->getMethod() == "POST"){
+                $form->handleRequest($request);
+                $modifyConntact = $form->getData();
+                $idModifyConntact = $modifyConntact->getId();
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($modifyConntact);
+                $em->flush();
+                return $this->redirectToRoute("/$idModifyConntact");
+            }
+        // return form to create form view 
+        return $this->render('ConntactBookBundle:User:formUser.html.twig', array(
+        "form" => $form->createView(), "title" => "Modify Conntact"
+        ));
+    }
+    
+    
+    
+    
     
 }
